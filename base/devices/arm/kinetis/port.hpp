b731f1ce46910d2bcd8b013d4a769835e8faa02a
siyikalender <selcuk.iyikalender@gmail.com>
2018-09-21 13:10:47 +0300

    Add read and write functions for port peripheral. Update the gpio example. Fix the build paths in k60d10.defs file

--- base/devices/arm/kinetis/port.hpp
+++ base/devices/arm/kinetis/port.hpp
@@ -81,9 +81,9 @@ inline void set_direction(port<T>,
 }
 
 template<typename T>
-inline void set_direction(port<T>, const uint32_t p_dir)
+inline void set_direction(port<T>, const uint32_t p_value)
 {
-  port<T>::gpio_addr()->PDDR = p_dir;
+  port<T>::gpio_addr()->PDDR = p_value;
 }
 
 template<typename T>
@@ -102,6 +102,18 @@ template<typename T>
 inline void toggle(port<T>, const uint32_t p_value)
 {
   port<T>::gpio_addr()->PTOR = p_value;
+}
+
+template<typename T>
+inline uint32_t read(port<T>)
+{
+  return port<T>::gpio_addr()->PDIR;
+}
+
+template<typename T>
+inline void write(port<T>, const uint32_t p_value)
+{
+  port<T>::gpio_addr()->PDOR = p_value;
 }
 
 // options for port configuration
