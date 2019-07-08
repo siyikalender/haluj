080e9fba68992c0ad8814bbec9ef3fb63bb4309c
siyikalender <selcuk.iyikalender@gmail.com>
2019-07-09 01:26:29 +0300

    Add namespace definitions

--- base/utility.hpp
+++ base/utility.hpp
@@ -3,6 +3,12 @@
 
 #include <cstdint>
 
+namespace haluj
+{
+
+namespace base
+{
+
 template<typename T, std::size_t N>
 constexpr std::size_t array_size(const T (&arr)[N])
 {
@@ -15,5 +21,9 @@ inline T limit(const T p_value, const T p_lower, const T p_upper)
   return (p_value < p_lower) ? p_lower : (
             (p_value > p_upper) ? p_upper : p_value);
 }
+
+} // namespace base
+
+} // namespace haluj
 
 #endif
